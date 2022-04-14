085d8983524e9dadaf13f2a4892438f55cb922cd
r_clementz <rie.h620@gmail.com>
2022-04-14 14:28:07 +0200

    Squashed commit of the following:
    commit be43150bd935b7d7c11dbd250b4447974591bf9e
    Author: r_clementz <rie.h620@gmail.com>
    Date:   Thu Apr 14 12:41:04 2022 +0200
    
        renamed feature file
    
    commit cda454d1fdc9ff5d9a1a93e54525452231cf9556
    Author: r_clementz <rie.h620@gmail.com>
    Date:   Thu Apr 14 11:23:54 2022 +0200
    
        file created
    
    commit 005beac1395fb9e8f69c3e4d0db2ab7966b58bef
    Author: r_clementz <rie.h620@gmail.com>
    Date:   Thu Apr 14 10:47:02 2022 +0200
    
    commit ca4e6888150bb0909551b7bfd9a8a95fdd47108f
    Author: r_clementz <rie.h620@gmail.com>
    Date:   Thu Apr 14 10:29:59 2022 +0200
    
        404.html fiexed, empty shopping cart works!
    
    commit ebb4715514615074fd7e45115be6f5eee32524a4
    Author: katarina-pa <kinesenochtrollet@gmail.com>
    Date:   Wed Apr 13 12:58:54 2022 +0200
    
        js command to emptyCart, needs further fix
    
    commit 11f170742682c177e69dcd45cca0dfe4e391c16b
    Author: r_clementz <rie.h620@gmail.com>
    Date:   Wed Apr 13 12:21:42 2022 +0200
    
        function to empty cart added

--- frontend/App.js
+++ frontend/App.js
@@ -6,7 +6,7 @@ class App {
   
     async start() {
   
-      // check if the user is logged in
+      // check if the user is  in
       let rawResult = await fetch('/api/login');
       let result = await rawResult.json();
   
@@ -40,16 +40,6 @@ class App {
       listen('click', '.closeCart', () => {
         grabEl('.shoppingCart').style.display = 'none';
       });
-
-      listen('click', '.emptyCart', () => {
-        // reloads the page to empty cart--need method to actually remove
-        location.reload();
-        alert('You have removed your selected items');
-      });
-
-      listen('click', '.checkoutOrder', () => {
-        // need to add method for checkout button found in ShoppingCart.js
-      });
   
       listen('click', '.register', () => {
         grabEl('.overlay').style.display = 'block';
@@ -78,12 +68,20 @@ class App {
         grabEl('.overlay').style.display = 'none';
         grabEl('.loginModal').style.display = 'none';
       });
-      //Empty the cart
-      listen('click', '.emptyCart',()=>{
-        this.productList=[]
-      })
-      //Send the order 
-      //listen ('click','.checkoutOrder', ()=>{});
+
+      listen('click', '.emptyCart', () => {
+        // reloads the page to empty cart--need method to actually remove
+        alert('You have removed your selected items');
+        this.productList.shoppingCart.empty();
+        //attempt to empty the cart by creating a new list & thus disregarding the old
+        //did NOT work
+        //this.productList=[];        
+      });
+
+      listen('click', '.checkoutOrder', () => {
+        // need to add method for checkout button found in ShoppingCart.js
+      });
+      
   
       LoginAndRegister.addEvents();
   
