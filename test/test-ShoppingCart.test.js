bd97264d8189583d8961b26d709fe99d5228cb37
BohemianDoor23 <91262770+PhilipV01@users.noreply.github.com>
2022-05-02 16:13:47 +0200

    Update test-ShoppingCart.test.js
    added test for shoppingCart(does not work)

--- test/test-ShoppingCart.test.js
+++ test/test-ShoppingCart.test.js
@@ -1,11 +1,18 @@
-const { TestCaseHookDefinition } = require('@cucumber/cucumber');
 const { expect } = require('@jest/globals');
 
 const ShoppingCart = require('../frontend/ShoppingCart.js');
+const Product = require('../frontend/Product.js');
 
 describe('Test the ShoppinCart class', () => {
 
     test('After a product is added, size of cart should increase', () => {
-        let myShoppingCart = new ShoppingCart
+        let myShoppingCart = new ShoppingCart();
+        let myProduct = new Product(1, 'Broom', 299, 'Best broom');
+
+        expect(ShoppingCart.orderRows.length()).toBe(0);
+
+        ShoppingCart.add(1, myProduct)
+
+        expect(ShoppingCart.orderRows.length()).toBe(1);
     })
 });
