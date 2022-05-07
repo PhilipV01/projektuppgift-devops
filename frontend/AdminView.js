0afff0eda13c3fd87fe5f2bfd19ab5c93ecdef3a
r_clementz <rie.h620@gmail.com>
2022-05-07 13:55:24 +0200

    allow to go to bottom, admin view button added

--- frontend/AdminView.js
+++ frontend/AdminView.js
@@ -1,22 +1,34 @@
 class AdminView{
     constructor() { }
-    
-    async readDataFromDb() {
+    viewName = orderDetails;
+        
+    async readDataFromDb(name) {
+        name = viewName;
         // read the json data
-        let rawData = await fetch('/api/products');
+        let rawData = await fetch('/api/'+ name);
         // convert from json to a JavaScript data structure
         // data will be an array of generic objects
         let data = await rawData.json();
 
+
+    }
+    render()
+    
+    //Function to click the button to see views 
+    addAVButton() {
+        listen('click', '.aVButton', event => {
+            
+        })
     }
 }
+
 
 /*Viewname 
  orderDetails: each products and price for orders 
  orderDetailSubTotal
  orderSums: total price for eahc order 
  totalOrderSumHist : total price and products list for each order
- 
+
  
  
  */
